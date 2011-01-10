acd18f1f6dca03d1ae16369bddd6fefc9cac2b7b
Konrad Banachowicz <konradb3@gmail.com>
2011-01-10 22:55:50 +0100

    fix.

--- irp6_launch/run_fakeservo.sh
+++ irp6_launch/run_fakeservo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rosrun xacro xacro.py irp6p_fakeservo.xml.xacro -o example.xml
-RTT_COMPONENT_PATH=/opt/ros/cturtle/stacks/orocos_toolchain_ros/rtt/install/lib/orocos `rospack find ocl`/bin/deployer-gnulinux -s example.xml
+RTT_COMPONENT_PATH=`rospack find rtt`/install/lib/orocos `rospack find ocl`/bin/deployer-gnulinux -s example.xml
