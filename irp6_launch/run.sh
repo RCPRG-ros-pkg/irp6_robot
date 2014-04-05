628025bfcea43ddc0fb8cc52a81260debc99b3c4
Konrad Banachowicz <konradb3@gmail.com>
2014-04-05 09:19:12 +0200

    cleanup

--- irp6_launch/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-RTT_COMPONENT_PATH=`rospack find rtt`/install/lib/orocos `rospack find ocl`/bin/deployer-gnulinux -s irp6p.xml
--- irp6_launch/run_fakeservo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rosrun xacro xacro.py irp6p_fakeservo.xml.xacro -o example.xml
-RTT_COMPONENT_PATH=`rospack find rtt`/install/lib/orocos `rospack find ocl`/bin/deployer-gnulinux -s example.xml
