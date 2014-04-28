6988c970343544eff1feba24e14c6ccc4f739000
Tomasz Winiarski <tomrobotics@gmail.com>
2014-04-28 17:57:10 +0200

    irp6p forward kineamtic in progress

--- irp6_kinematic/src/Irp6pForwardKinematic.cpp
+++ irp6_kinematic/src/Irp6pForwardKinematic.cpp
@@ -1,13 +1,13 @@
 #include <rtt/Component.hpp>
 
 #include "Irp6pForwardKinematic.h"
+#include "eigen_conversions/eigen_msg.h"
 
+Irp6pForwardKinematic::Irp6pForwardKinematic(const std::string& name)
+    : RTT::TaskContext(name, PreOperational) {
 
-Irp6pForwardKinematic::Irp6pForwardKinematic(const std::string& name) : RTT::TaskContext(name, PreOperational) {
-
-	this->ports()->addPort("JointPosition", port_joint_position_);
-	this->ports()->addPort("OutputPose", port_output_pose_);
-
+  this->ports()->addPort("JointPosition", port_joint_position_);
+  this->ports()->addPort("OutputPose", port_output_pose_);
 
 }
 
@@ -17,15 +17,27 @@ Irp6pForwardKinematic::~Irp6pForwardKinematic() {
 
 bool Irp6pForwardKinematic::configureHook() {
 
-	return true;
+  joint_position_.resize(6);
+  return true;
 }
 
 void Irp6pForwardKinematic::updateHook() {
 
+  if (port_joint_position_.read(joint_position_) == RTT::NewData) {
+
+    Eigen::Affine3d trans;
+    geometry_msgs::Pose pos;
+
+    direct_kinematics_transform(joint_position_, &trans);
+
+    tf::poseEigenToMsg(trans, pos);
+    port_output_pose_.write(pos);
+  }
 }
 
-void Irp6pForwardKinematic::mp2i(const double* motors, double* joints)
-{
+void Irp6pForwardKinematic::direct_kinematics_transform(
+    const Eigen::VectorXd& local_current_joints,
+    Eigen::Affine3d* local_current_end_effector_frame) {
 
 }
 
--- irp6_kinematic/src/Irp6pForwardKinematic.h
+++ irp6_kinematic/src/Irp6pForwardKinematic.h
@@ -7,20 +7,22 @@
 
 #include <Eigen/Dense>
 
-class Irp6pForwardKinematic: public RTT::TaskContext {
-public:
-	Irp6pForwardKinematic(const std::string& name);
-	virtual ~Irp6pForwardKinematic();
+class Irp6pForwardKinematic : public RTT::TaskContext {
+ public:
+  Irp6pForwardKinematic(const std::string& name);
+  virtual ~Irp6pForwardKinematic();
 
-	bool configureHook();
-	void updateHook();
-private:
-	void mp2i(const double* motors, double* joints);
+  bool configureHook();
+  void updateHook();
+ private:
+  void direct_kinematics_transform(
+      const Eigen::VectorXd& local_current_joints,
+      Eigen::Affine3d* local_current_end_effector_frame);
 
-	RTT::InputPort<Eigen::VectorXd > port_joint_position_;
-	RTT::OutputPort<geometry_msgs::Pose> port_output_pose_;
+  RTT::InputPort<Eigen::VectorXd> port_joint_position_;
+  RTT::OutputPort<geometry_msgs::Pose> port_output_pose_;
 
-	Eigen::VectorXd motor_position_, joint_position_;
+  Eigen::VectorXd joint_position_;
 };
 
 #endif /* Irp6pForwardKinematic */
