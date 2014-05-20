ab15483698842aab704395fef98d272ca8e998d3
Tomasz Winiarski <tomrobotics@gmail.com>
2014-05-20 17:25:25 +0200

    forward kineamtics tool addition

--- irp6_kinematic/src/Irp6pForwardKinematic.cpp
+++ irp6_kinematic/src/Irp6pForwardKinematic.cpp
@@ -7,7 +7,11 @@ Irp6pForwardKinematic::Irp6pForwardKinematic(const std::string& name)
     : RTT::TaskContext(name, PreOperational) {
 
   this->ports()->addPort("JointPosition", port_joint_position_);
-  this->ports()->addPort("OutputPose", port_output_pose_);
+  this->ports()->addPort("Tool", port_tool_);
+
+  this->ports()->addPort("WristOutputPose", port_output_wrist_pose_);
+  this->ports()->addPort("EndEffectorOutputPose",
+                         port_output_end_effector_pose_);
 
 }
 
@@ -28,6 +32,13 @@ bool Irp6pForwardKinematic::configureHook() {
 
   joint_position_.resize(6);
   local_current_joints_tmp_.resize(6);
+
+  if (port_tool_.read(tool_msgs_) == RTT::NewData) {
+
+    return false;
+
+  }
+
   return true;
 }
 
@@ -35,13 +46,25 @@ void Irp6pForwardKinematic::updateHook() {
 
   if (port_joint_position_.read(joint_position_) == RTT::NewData) {
 
-    Eigen::Affine3d trans;
-    geometry_msgs::Pose pos;
+    port_tool_.read(tool_msgs_);
 
-    direct_kinematics_transform(joint_position_, &trans);
+    Eigen::Affine3d tool;
+    Eigen::Affine3d end_effector_pose;
+    Eigen::Affine3d wrist_pose;
+    geometry_msgs::Pose wrist_pose_msgs;
+    geometry_msgs::Pose end_effector_pose_msgs;
 
-    tf::poseEigenToMsg(trans, pos);
-    port_output_pose_.write(pos);
+    tf::poseMsgToEigen(tool_msgs_, tool);
+
+    direct_kinematics_transform(joint_position_, &wrist_pose);
+
+    end_effector_pose = wrist_pose * tool;
+
+    tf::poseEigenToMsg(wrist_pose, wrist_pose_msgs);
+    tf::poseEigenToMsg(end_effector_pose, end_effector_pose_msgs);
+
+    port_output_wrist_pose_.write(wrist_pose_msgs);
+    port_output_end_effector_pose_.write(end_effector_pose_msgs);
   }
 }
 
@@ -75,13 +98,15 @@ void Irp6pForwardKinematic::direct_kinematics_transform(
   (*local_current_end_effector_frame)(0, 1) = -(c1 * s4 * c5 + s1 * s5) * s6
       + c1 * c4 * c6;  //OX
   (*local_current_end_effector_frame)(0, 2) = c1 * s4 * s5 - s1 * c5;  //AX
-  (*local_current_end_effector_frame)(0, 3) = c1 * (a2 * c2 + a3 * c3 + d5 * c4);  //PX
+  (*local_current_end_effector_frame)(0, 3) = c1
+      * (a2 * c2 + a3 * c3 + d5 * c4);  //PX
   (*local_current_end_effector_frame)(1, 0) = (s1 * s4 * c5 - c1 * s5) * c6
       + s1 * c4 * s6;  //NY2
   (*local_current_end_effector_frame)(1, 1) = -(s1 * s4 * c5 - c1 * s5) * s6
       + s1 * c4 * c6;  //OY
   (*local_current_end_effector_frame)(1, 2) = s1 * s4 * s5 + c1 * c5;  //AY
-  (*local_current_end_effector_frame)(1, 3) = s1 * (a2 * c2 + a3 * c3 + d5 * c4);  //PY
+  (*local_current_end_effector_frame)(1, 3) = s1
+      * (a2 * c2 + a3 * c3 + d5 * c4);  //PY
   (*local_current_end_effector_frame)(2, 0) = c4 * c5 * c6 - s4 * s6;  //NZ
   (*local_current_end_effector_frame)(2, 1) = -c4 * c5 * s6 - s4 * c6;  //OZ
   (*local_current_end_effector_frame)(2, 2) = c4 * s5;  //AZ
