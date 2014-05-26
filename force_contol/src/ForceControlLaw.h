746a36f8ad74e8c14b2903518f29016f261fa08c
Tomasz Winiarski <tomrobotics@gmail.com>
2014-05-26 18:34:34 +0200

    force control in progress

--- force_contol/src/ForceControlLaw.h
+++ force_contol/src/ForceControlLaw.h
@@ -7,6 +7,8 @@
 #include "geometry_msgs/Wrench.h"
 
 #include <Eigen/Dense>
+#include "ForceTrans.h"
+
 
 class ForceControlLaw : public RTT::TaskContext {
  public:
@@ -25,6 +27,38 @@ class ForceControlLaw : public RTT::TaskContext {
   RTT::InputPort<geometry_msgs::Wrench> port_current_wrench_;
 
   geometry_msgs::Pose current_pose_;
+
+  // MRROC++
+
+  /*!
+   * \brief Info if the force sensor test mode is active.
+   *
+   * It is taken from configuration data.
+   */
+  bool force_sensor_test_mode;
+
+  bool is_reading_ready;  // czy jakikolwiek odczyt jest gotowy?
+
+  // nazwa czujnika
+  short force_sensor_name;
+
+  // is sensor_frame right turn
+  bool is_right_turn_frame;
+
+  // force_sensor_frame related to wrist frame
+  KDL::Frame force_sensor_frame;
+
+  ForceTrans *gravity_transformation;  // klasa likwidujaca wplyw grawitacji na czujnik
+
+  // ft_table used in get_reading and get_particualr_reading
+  KDL::Wrench ft_table;
+
+  KDL::Wrench force_constraints;
+
+  KDL::Frame tool_mass_center_translation;
+
+  //! czy czujnik skonfigurowany?
+  bool is_sensor_configured;
 
 };
 
