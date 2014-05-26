0216f74c6373f165ca2662cbdfe4d51ee9825714
Tomasz Winiarski <tomrobotics@gmail.com>
2014-05-26 17:52:36 +0200

    force control in progress

--- force_contol/src/ForceTrans.cpp
+++ force_contol/src/ForceTrans.cpp
@@ -21,75 +21,78 @@ ForceTrans::ForceTrans(const short l_force_sensor_name,
   tool_weight = weight;
   gravity_arm_in_wrist = point_of_gravity;
   synchro(init_frame);
-  /* Temporary removal
-   // defineTool(init_frame, weight, point_of_gravity);
-   */
+  defineTool(init_frame, weight, point_of_gravity);
 
   initialized = true;
 
 }
 
-/*
- void ForceTrans::defineTool(const lib::Homog_matrix & init_frame, const double weight, const lib::K_vector & point_of_gravity)
- {
- tool_weight = weight;
- gravity_arm_in_wrist = point_of_gravity;
+void ForceTrans::defineTool(const KDL::Frame & init_frame, const double weight,
+                            const KDL::Vector & point_of_gravity) {
 
- gravity_force_torque_in_base = lib::Ft_vector(0.0, 0.0, -tool_weight, 0.0, 0.0, 0.0);
+  tool_weight = weight;
+  gravity_arm_in_wrist = point_of_gravity;
 
- // orientacja koncowki manipulatora bez narzedzia
- lib::Homog_matrix current_orientation(init_frame.return_with_with_removed_translation());
+  gravity_force_torque_in_base = KDL::Wrench(
+      KDL::Vector(0.0, 0.0, -tool_weight), KDL::Vector(0.0, 0.0, 0.0));
 
- // sila reakcji w ukladzie czujnika z orientacja bazy
- lib::Ft_vector gravity_force_torque_in_sensor(lib::Xi_f(!current_orientation) * gravity_force_torque_in_base);
+// orientacja koncowki manipulatora bez narzedzia
+  KDL::Frame current_orientation(init_frame.M, KDL::Vector(0.0, 0.0, 0.0));
 
- // macierz narzedzia wzgledem nadgarstka
- lib::Homog_matrix tool_mass_center_translation(point_of_gravity[0], point_of_gravity[1], point_of_gravity[2]);
- ft_tool_mass_center_translation = lib::Xi_f(tool_mass_center_translation);
+// sila reakcji w ukladzie czujnika z orientacja bazy
+  KDL::Wrench gravity_force_torque_in_sensor = current_orientation.Inverse()
+      * gravity_force_torque_in_base;
 
- // sila reakcji w ukladzie nadgarstka z orientacja bazy
- reaction_force_torque_in_wrist = -(ft_tool_mass_center_translation * gravity_force_torque_in_sensor);
+// macierz narzedzia wzgledem nadgarstka
+  KDL::Frame tool_mass_center_translation(KDL::Rotation(), point_of_gravity);
 
- }
+  ft_tool_mass_center_translation = tool_mass_center_translation;
 
- // zwraca sily i momenty sil w w ukladzie z orientacja koncowki manipulatory bez narzedzia
- lib::Ft_vector ForceTrans::getForce(const lib::Ft_vector _inputForceTorque, const lib::Homog_matrix curr_frame)
- {
+// sila reakcji w ukladzie nadgarstka z orientacja bazy
+  reaction_force_torque_in_wrist = -(ft_tool_mass_center_translation
+      * gravity_force_torque_in_sensor);
 
- lib::Ft_vector inputForceTorque = _inputForceTorque;
+}
 
- if (!is_right_turn_frame) {
+// zwraca sily i momenty sil w w ukladzie z orientacja koncowki manipulatory bez narzedzia
+KDL::Wrench ForceTrans::getForce(const KDL::Wrench _inputForceTorque,
+                                 const KDL::Frame curr_frame) {
 
- inputForceTorque[2] = -inputForceTorque[2];
- inputForceTorque[5] = -inputForceTorque[5];
- }
+  KDL::Wrench inputForceTorque = _inputForceTorque;
 
- if (initialized) {
+  if (!is_right_turn_frame) {
 
- // sprowadzenie wejsciowych, zmierzonych sil i momentow sil z ukladu czujnika do ukladu nadgarstka
- lib::Ft_vector input_force_torque(ft_tr_sensor_in_wrist * inputForceTorque);
+    inputForceTorque[2] = -inputForceTorque[2];
+    inputForceTorque[5] = -inputForceTorque[5];
+  }
 
- // sprowadzenie odczytow sil do ukladu czujnika przy zalozeniu ze uklad chwytaka ma te sama orientacje
- // co uklad narzedzia
- lib::Ft_vector gravity_force_torque_in_sensor(lib::Xi_star(!curr_frame) * gravity_force_torque_in_base);
+  if (initialized) {
 
- // finalne przeksztalcenie (3.30 z doktoratu TW)
- lib::Ft_vector output_force_torque(input_force_torque
- - (ft_tool_mass_center_translation * gravity_force_torque_in_sensor) - reaction_force_torque_in_wrist);
+    // sprowadzenie wejsciowych, zmierzonych sil i momentow sil z ukladu czujnika do ukladu nadgarstka
+    KDL::Wrench input_force_torque = ft_tr_sensor_in_wrist * inputForceTorque;
 
- // sprowadzenie sily w ukladzie nadgarstka do orientacji ukladu bazowego
- output_force_torque = lib::Xi_star(curr_frame) * Ft_vector(-output_force_torque);
+    // sprowadzenie odczytow sil do ukladu czujnika przy zalozeniu ze uklad chwytaka ma te sama orientacje
+    // co uklad narzedzia
+    KDL::Wrench gravity_force_torque_in_sensor = (curr_frame.Inverse()).M
+        * gravity_force_torque_in_base;
 
- return output_force_torque;
- }
- return 0;
- }
- */
+    // finalne przeksztalcenie (3.30 z doktoratu TW)
+    KDL::Wrench output_force_torque = input_force_torque
+        - ft_tool_mass_center_translation * gravity_force_torque_in_sensor
+        - reaction_force_torque_in_wrist;
+
+    // sprowadzenie sily w ukladzie nadgarstka do orientacji ukladu bazowego
+    output_force_torque = curr_frame.M * (-output_force_torque);
+
+    return output_force_torque;
+  }
+
+  return KDL::Wrench();
+}
+
 void ForceTrans::synchro(const KDL::Frame & init_frame) {
   if (initialized) {
-    /* Temporary removal
     defineTool(init_frame, tool_weight, gravity_arm_in_wrist);
-    */
   }
 }
 
--- force_contol/src/ForceTrans.h
+++ force_contol/src/ForceTrans.h
@@ -3,38 +3,38 @@
 
 #include "kdl/frames.hpp"
 
-class ForceTrans
-{
+class ForceTrans {
 
-protected:
+ protected:
 
-	const short force_sensor_name;
+  const short force_sensor_name;
 
-	bool initialized;
-	double tool_weight;
+  bool initialized;
+  double tool_weight;
 
-	KDL::Wrench gravity_force_torque_in_base;
-	KDL::Wrench reaction_force_torque_in_wrist;
-	KDL::Vector gravity_arm_in_wrist;
+  KDL::Wrench gravity_force_torque_in_base;
+  KDL::Wrench reaction_force_torque_in_wrist;
+  KDL::Vector gravity_arm_in_wrist;
 
-	KDL::Frame sensor_frame;
+  KDL::Frame sensor_frame;
 
-	KDL::Frame ft_tool_mass_center_translation;
+  KDL::Frame ft_tool_mass_center_translation;
 
-	KDL::Frame ft_tr_sensor_in_wrist;
+  KDL::Frame ft_tr_sensor_in_wrist;
 
-	bool is_right_turn_frame;
+  bool is_right_turn_frame;
 
-public:
-	ForceTrans(const short l_force_sensor_name, const KDL::Frame & init_frame, const KDL::Frame & s_frame, const double weight, const KDL::Vector & point_of_gravity, bool _is_right_turn_frame);
+ public:
+  ForceTrans(const short l_force_sensor_name, const KDL::Frame & init_frame,
+             const KDL::Frame & s_frame, const double weight,
+             const KDL::Vector & point_of_gravity, bool _is_right_turn_frame);
   void synchro(const KDL::Frame & init_frame);
+  void defineTool(const KDL::Frame & init_frame, const double weight,
+                  const KDL::Vector & point_of_gravity);
 
-	/*
-	void defineTool(const lib::Homog_matrix & init_frame, const double weight, const lib::K_vector & point_of_gravity);
-	lib::Ft_vector getForce(const lib::Ft_vector _inputForceTorque, const lib::Homog_matrix curr_frame);
+  KDL::Wrench getForce(const KDL::Wrench _inputForceTorque,
+                          const KDL::Frame curr_frame);
 
-	EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-	*/
 };
 
 #endif
