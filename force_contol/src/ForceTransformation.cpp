677766ff0979b7f2c260e5ade9edb40bd7092afc
Tomasz Winiarski <tomrobotics@gmail.com>
2014-05-27 12:49:27 +0200

    force control in progress

--- force_contol/src/ForceTransformation.cpp
+++ force_contol/src/ForceTransformation.cpp
@@ -2,6 +2,7 @@
 
 #include "ForceTransformation.h"
 #include "eigen_conversions/eigen_msg.h"
+#include "kdl_conversions/kdl_msg.h"
 
 ForceTransformation::ForceTransformation(const std::string& name)
     : RTT::TaskContext(name, PreOperational),
@@ -11,7 +12,7 @@ ForceTransformation::ForceTransformation(const std::string& name)
       gravity_transformation(NULL),
       is_sensor_configured(false) {
 
-  this->ports()->addPort("CurrentPose", port_current_pose_);
+  this->ports()->addPort("CurrentWristPose", port_current_wrist_pose_);
   this->ports()->addPort("Tool", port_tool_);
 
   this->ports()->addPort("CurrentWrench", port_current_wrench_);
@@ -29,9 +30,85 @@ bool ForceTransformation::configureHook() {
 }
 
 bool ForceTransformation::startHook() {
-  if (port_current_pose_.read(current_pose_) == RTT::NoData) {
+  if (port_current_wrist_pose_.read(current_wrist_pose_) == RTT::NoData) {
     return false;
   }
+
+  is_sensor_configured = true;
+
+  // force offset determination
+  if (!force_sensor_test_mode) {
+    // read current force
+    geometry_msgs::Wrench current_wrench;
+    port_current_wrench_.read(current_wrench);
+
+    tf::wrenchMsgToKDL(current_wrench, force_offset);
+
+  }
+  /*
+   // polozenie kisci bez narzedzia wzgledem bazy
+   lib::Homog_matrix current_frame =
+   master.servo_current_frame_wo_tool_dp.read();  // FORCE Transformation by Slawomir Bazant
+
+   if (!gravity_transformation)  // nie powolano jeszcze obiektu
+   {
+
+   // zczytanie sil maksymalnych
+   if (master.config.exists("force_constraints")) {
+   char *tmp = strdup(
+   master.config.value < std::string > ("force_constraints").c_str());
+   char* toDel = tmp;
+   for (int i = 0; i < 6; i++) {
+   force_constraints[i] = strtod(tmp, &tmp);
+   }
+   free(toDel);
+   }
+
+   lib::Xyz_Angle_Axis_vector tab;
+   //zczytanie polozenia czujnika sily wzgledem nadgarstka
+   if (master.config.exists("force_sensor_in_wrist")) {
+   char *tmp = strdup(
+   master.config.value < std::string
+   > ("force_sensor_in_wrist").c_str());
+   char* toDel = tmp;
+   for (int i = 0; i < 6; i++) {
+   tab[i] = strtod(tmp, &tmp);
+   }
+   force_sensor_frame = lib::Homog_matrix(tab);
+   free(toDel);
+   }
+
+   // zczytanie ciezaru narzedzia
+   double weight = master.config.value<double>("weight");
+
+   next_force_tool_weight = weight;
+
+   // polozenie srodka ciezkosci narzedzia wzgledem nadgarstka
+   double point[3];
+   char *tmp = strdup(
+   master.config.value < std::string
+   > ("default_mass_center_in_wrist").c_str());
+   char* toDel = tmp;
+   for (int i = 0; i < 3; i++)
+   point[i] = strtod(tmp, &tmp);
+   free(toDel);
+
+   lib::K_vector pointofgravity(point);
+
+   tool_mass_center_translation = lib::Homog_matrix(point[0], point[1],
+   point[2]);
+
+   gravity_transformation = new lib::ForceTrans(force_sensor_name,
+   current_frame,
+   force_sensor_frame, weight,
+   pointofgravity,
+   is_right_turn_frame);
+   } else {
+   gravity_transformation->synchro(current_frame);
+   }
+
+   */
+
   return true;
 }
 
--- force_contol/src/ForceTransformation.h
+++ force_contol/src/ForceTransformation.h
@@ -21,13 +21,13 @@ class ForceTransformation : public RTT::TaskContext {
 
  private:
 
-  RTT::InputPort<geometry_msgs::Pose> port_current_pose_;
+  RTT::InputPort<geometry_msgs::Pose> port_current_wrist_pose_;
   RTT::InputPort<geometry_msgs::Pose> port_tool_;
 
   RTT::InputPort<geometry_msgs::Wrench> port_current_wrench_;
   RTT::OutputPort<geometry_msgs::Wrench> port_output_wrench_;
 
-  geometry_msgs::Pose current_pose_;
+  geometry_msgs::Pose current_wrist_pose_;
 
   // MRROC++
 
@@ -52,6 +52,8 @@ class ForceTransformation : public RTT::TaskContext {
   KDL::Wrench ft_table;
 
   KDL::Wrench force_constraints;
+
+  KDL::Wrench force_offset;
 
   KDL::Frame tool_mass_center_translation;
 
