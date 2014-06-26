7500d37a5a55cbadca1c1f7839bfee52a72bc745
Tomasz Winiarski <tomrobotics@gmail.com>
2014-06-26 19:16:06 +0200

    reagulator parametrization

--- irp6_regulator/src/IRp6Regulator.cpp
+++ irp6_regulator/src/IRp6Regulator.cpp
@@ -8,16 +8,21 @@
 
 #include "IRp6Regulator.h"
 
-IRp6Regulator::IRp6Regulator(const std::string& name) :
-		TaskContext(name), posInc_in("posInc_in"), deltaInc_in("deltaInc_in"), computedPwm_out(
-				"computedPwm_out") {
+IRp6Regulator::IRp6Regulator(const std::string& name)
+    : TaskContext(name),
+      posInc_in("posInc_in"),
+      deltaInc_in("deltaInc_in"),
+      computedPwm_out("computedPwm_out") {
 
-	this->addEventPort(posInc_in).doc("Receiving a value of position step");
-	this->addPort(deltaInc_in).doc("Receiving a value of measured increment.");
-	this->addPort(computedPwm_out).doc("Sending value of calculated pwm.");
+  this->addEventPort(posInc_in).doc("Receiving a value of position step");
+  this->addPort(deltaInc_in).doc("Receiving a value of measured increment.");
+  this->addPort(computedPwm_out).doc("Sending value of calculated pwm.");
 
-	this->addProperty("number_of_drives", number_of_drives).doc(
-			"Number of drives in robot");
+  this->addProperty("number_of_drives", number_of_drives_).doc(
+      "Number of drives in robot");
+  this->addProperty("A", A_).doc("");
+  this->addProperty("BB0", BB0_).doc("");
+  this->addProperty("BB1", BB1_).doc("");
 }
 
 IRp6Regulator::~IRp6Regulator() {
@@ -25,29 +30,35 @@ IRp6Regulator::~IRp6Regulator() {
 }
 
 bool IRp6Regulator::configureHook() {
+  if (A_.size() != number_of_drives_ || BB0_.size() != number_of_drives_
+      || BB1_.size() != number_of_drives_) {
+    log(Error) << "Size of parameters is different than number of drives"
+               << endlog();
+    return false;
+  }
 
-	for (int i = 0; i < number_of_drives; i++) {
-		regulator[i].reset();
-		regulator[i].setParam(A[i], BB0[i], BB1[i]);
-	}
-	return true;
+  for (int i = 0; i < number_of_drives_; i++) {
+    regulator[i].reset();
+    regulator[i].setParam(A_[i], BB0_[i], BB1_[i]);
+  }
+  return true;
 }
 
 void IRp6Regulator::updateHook() {
-	if (NewData == posInc_in.read(posIncData)
-			&& NewData == deltaInc_in.read(deltaIncData)) {
-		computedPwm_out.write(computePwmValue(posIncData, deltaIncData));
-	}
+  if (NewData == posInc_in.read(posIncData)
+      && NewData == deltaInc_in.read(deltaIncData)) {
+    computedPwm_out.write(computePwmValue(posIncData, deltaIncData));
+  }
 }
 
 std::vector<double> IRp6Regulator::computePwmValue(
-		const std::vector<double>& posInc, const std::vector<int>& deltaInc) {
-	std::vector<double> ret(number_of_drives);
-	for (int i = 0; i < number_of_drives; i++) {
-		ret[i] = regulator[i].doServo(posInc[i], deltaInc[i]);
-	}
+    const std::vector<double>& posInc, const std::vector<int>& deltaInc) {
+  std::vector<double> ret(number_of_drives_);
+  for (int i = 0; i < number_of_drives_; i++) {
+    ret[i] = regulator[i].doServo(posInc[i], deltaInc[i]);
+  }
 
-	return ret;
+  return ret;
 }
 
 ORO_CREATE_COMPONENT(IRp6Regulator)
--- irp6_regulator/src/IRp6Regulator.h
+++ irp6_regulator/src/IRp6Regulator.h
@@ -3,43 +3,39 @@
 
 #include "Regulator.h"
 
-//IRP6
-const double A[6] = { 0.412429378531, 0.655629139073, 0.315789473684,
-		0.548946716233, 0.391982182628, 0.3 };
-const double BB0[6] = { 2.594932 * 0.6, 1.030178 * 0.6, 1.997464 * 0.6, 1.576266
-		* 0.4, 1.114648 * 0.4, 1.364 * 0.4 };
-const double BB1[6] = { 2.504769 * 0.6, 0.986142 * 0.6, 1.904138 * 0.6, 1.468599
-		* 0.4, 1.021348 * 0.4, 1.264 * 0.4 };
-
 using namespace RTT;
 
-class IRp6Regulator: public RTT::TaskContext {
+class IRp6Regulator : public RTT::TaskContext {
 
-private:
+ private:
 
-	InputPort<std::vector<double> > posInc_in;
-	InputPort<std::vector<int> > deltaInc_in;
+  InputPort<std::vector<double> > posInc_in;
+  InputPort<std::vector<int> > deltaInc_in;
 
-	OutputPort<std::vector<double> > computedPwm_out;
+  OutputPort<std::vector<double> > computedPwm_out;
 
-	std::vector<double> posIncData;
-	std::vector<int> deltaIncData;
+  std::vector<double> posIncData;
+  std::vector<int> deltaIncData;
 
-	int number_of_drives;
+  Regulator regulator[6];
 
-	Regulator regulator[6];
+  // Properties
+  int number_of_drives_;
+  std::vector<double> A_;
+  std::vector<double> BB0_;
+  std::vector<double> BB1_;
 
-public:
+ public:
 
-	IRp6Regulator(const std::string& name);
-	~IRp6Regulator();
+  IRp6Regulator(const std::string& name);
+  ~IRp6Regulator();
 
-private:
+ private:
 
-	bool configureHook();
-	void updateHook();
-	std::vector<double> computePwmValue(const std::vector<double>& posInc,
-			const std::vector<int>& deltaInc);
+  bool configureHook();
+  void updateHook();
+  std::vector<double> computePwmValue(const std::vector<double>& posInc,
+                                      const std::vector<int>& deltaInc);
 
 };
 #endif
