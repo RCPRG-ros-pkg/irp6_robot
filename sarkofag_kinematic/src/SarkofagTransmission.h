287dc90ec68d91ac8b4af089622f2fe48fe00b4c
Bartosz Swistak <bswistak@sarkofag.ia.pw.edu.pl>
2014-07-16 19:15:44 +0200

    Sarkofag added

---
+++ sarkofag_kinematic/src/SarkofagTransmission.h
@@ -0,0 +1,15 @@
+#ifndef SarkofagTransmission_H_
+#define SarkofagTransmission_H_
+
+const double GEAR[1] = {-158.0};
+const double SYNCHRO_MOTOR_POSITION[1] = {-15.9};
+const double THETA[1] = {0.0};
+
+const double  SYNCHRO_JOINT_POSITION[1] = { SYNCHRO_MOTOR_POSITION[0] - GEAR[0] * THETA[0]};
+
+const int ENC_RES[1] = {4000};
+
+const double LOWER_MOTOR_LIMIT[1] = { -470};
+const double UPPER_MOTOR_LIMIT[1] = { 450};
+
+#endif /* SarkofagTransmission_H_ */
