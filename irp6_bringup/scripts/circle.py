60043819ed092fec286d77505fd89e2f4470bafd
Tomasz Winiarski <tomrobotics@gmail.com>
2014-07-17 14:33:35 +0200

    conveyor addition

--- irp6_bringup/scripts/circle.py
+++ irp6_bringup/scripts/circle.py
@@ -10,6 +10,10 @@ from numpy.linalg import *
 from controller_manager_msgs.srv import *
 from geometry_msgs.msg import *
 from cartesian_trajectory_msgs.msg import *
+from std_msgs.msg import *
+from diagnostic_msgs.msg import *
+from trajectory_msgs.msg import *
+from control_msgs.msg import *
 
 def generateCircle(P1, P2, P3, T):
   
@@ -51,7 +55,27 @@ rospy.init_node('multi_trajectory')
 rospy.wait_for_service('/controller_manager/switch_controller')
 conmanSwitch = rospy.ServiceProxy('/controller_manager/switch_controller', SwitchController)
 
-conmanSwitch(['Irp6pmPoseInt'], [], True)
+
+conmanSwitch(['Irp6pmSplineTrajectoryGeneratorJoint'], [], True)
+  
+joint_client = actionlib.SimpleActionClient('/irp6p_arm/spline_trajectory_action_joint', FollowJointTrajectoryAction)
+joint_client.wait_for_server()
+
+print 'server ok'
+
+goal = FollowJointTrajectoryGoal()
+goal.trajectory.joint_names = ['joint1', 'joint2', 'joint3', 'joint4', 'joint5', 'joint6']
+goal.trajectory.points.append(JointTrajectoryPoint([0.0, -1.5418065817051163, 0.0, 1.5, 1.57, -2.0], [0.0, 0.0, 0.0, 0.0, 0.0, 0.0], [], [], rospy.Duration(5.0)))
+goal.trajectory.header.stamp = rospy.get_rostime() + rospy.Duration(0.2)
+
+joint_client.send_goal(goal)
+
+joint_client.wait_for_result()
+command_result = joint_client.get_result()
+  
+
+
+conmanSwitch(['Irp6pmPoseInt'], ['Irp6pmSplineTrajectoryGeneratorJoint'], True)
 
 pose_client = actionlib.SimpleActionClient('/irp6p_arm/pose_trajectory', CartesianTrajectoryAction)
 pose_client.wait_for_server()
