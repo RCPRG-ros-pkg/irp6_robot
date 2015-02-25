62b7eee8c9164cab91391830fb6d79a60484e8a4
Bartosz Swistak <bartswis@gmail.com>
2015-02-25 17:28:20 +0100

    sarkofag fix

--- sarkofag_kinematic/src/SarkofagKinematic.h
+++
@@ -1,8 +0,0 @@
-// Copyright WUT 2014
-#ifndef SarkofagKinematic_H_
-#define SarkofagKinematic_H_
-
-const double z_offset_const = 0.95;
-const double a1_const = 0.455;
-
-#endif  // SarkofagKinematic_H_
