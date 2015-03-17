c44619f6aec0ddcd400778f092c68e975cc9bb1c
Tomasz Winiarski <tomrobotics@gmail.com>
2015-03-17 20:04:38 +0100

    force sensor unification

---
+++ irp6_force_sensor/src/ATI3084.h
@@ -0,0 +1,42 @@
+#ifndef ATI3084_H
+#define ATI3084_H
+
+#include <string>
+#include <comedilib.h>
+
+#include <rtt/TaskContext.hpp>
+#include <rtt/Port.hpp>
+#include <rtt/Property.hpp>
+
+#include <kdl/frames.hpp>
+#include "geometry_msgs/Wrench.h"
+#include "ForceSensor.h"
+
+
+#define USLEEP_MUX 50
+
+
+class ATI3084 : public ForceSensor {
+ public:
+  ATI3084(const std::string &name);
+
+  bool configureParticularSensorHook();
+
+
+ protected:
+  RTT::Property<std::string> device_prop_;
+
+ private:
+
+
+  lsampl_t maxdata_;
+  comedi_range *rangetype_;
+
+
+  void readData();
+
+
+
+};
+
+#endif // ATI3084_H
---
+++ irp6_force_sensor/src/ATI6284.h
@@ -0,0 +1,42 @@
+#ifndef ATI6284_H
+#define ATI6284_H
+
+#include <string>
+#include <comedilib.h>
+
+#include <rtt/TaskContext.hpp>
+#include <rtt/Port.hpp>
+#include <rtt/Property.hpp>
+
+#include <kdl/frames.hpp>
+
+#include "geometry_msgs/Wrench.h"
+#include "ForceSensor.h"
+
+
+
+
+
+class ATI6284 : public ForceSensor {
+ public:
+  ATI6284(const std::string &name);
+
+  bool configureParticularSensorHook();
+
+
+
+ protected:
+  RTT::Property<std::string> device_prop_;
+
+ private:
+
+  comedi_polynomial_t calib_ADC_;
+
+
+
+  void readData();
+
+
+};
+
+#endif // ATI6284_H
