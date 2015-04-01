2161bd8ad07aa54c6a5487abc2154336c549718b
Tomek Winiarski <tomrobotics@gmail.com>
2015-04-01 14:30:06 -0700

    fix

--- file_current_control/src/FileCurrentControl.h
+++ file_current_control/src/FileCurrentControl.h
@@ -1,17 +1,43 @@
+/*
+ * Copyright (c) 2014-2015, Robot Control and Pattern Recognition Group, Warsaw University of Technology.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Robot Control and Pattern Recognition Group,
+ *       Warsaw University of Technology nor the names of its contributors may
+ *       be used to endorse or promote products derived from this software
+ *       without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #ifndef FILECURRENTCONTROL_H_
 #define FILECURRENTCONTROL_H_
 
+#include <vector>
 #include <fstream>
 #include <string>
 
-using namespace std;
-using namespace RTT;
-
 class FileCurrentControl : public RTT::TaskContext {
-
  public:
-
-  FileCurrentControl(const std::string& name);
+  explicit FileCurrentControl(const std::string& name);
   ~FileCurrentControl();
 
   int doServo(double, int);
@@ -19,16 +45,15 @@ class FileCurrentControl : public RTT::TaskContext {
   void reset();
 
  private:
-
   bool configureHook();
   void updateHook();
 
-  InputPort<double> desired_position_;
-  InputPort<double> deltaInc_in;
-  InputPort<bool> synchro_state_in_;
+  RTT::InputPort<double> desired_position_;
+  RTT::InputPort<double> deltaInc_in;
+  RTT::InputPort<bool> synchro_state_in_;
 
-  OutputPort<double> computedPwm_out;
-  OutputPort<bool> emergency_stop_out_;
+  RTT::OutputPort<double> computedPwm_out;
+  RTT::OutputPort<bool> emergency_stop_out_;
 
   double desired_position_increment_;
   double desired_position_old_, desired_position_new_;
@@ -36,8 +61,8 @@ class FileCurrentControl : public RTT::TaskContext {
 
   bool synchro_state_old_, synchro_state_new_;
 
-  long update_hook_iteration_number_;
-  long new_position_iteration_number_;
+  int64_t update_hook_iteration_number_;
+  int64_t new_position_iteration_number_;
 
   // Properties
   int reg_number_;
@@ -51,7 +76,6 @@ class FileCurrentControl : public RTT::TaskContext {
   double eint_dif_;
   double max_desired_increment_;
   double enc_res_;
-
 
   double position_increment_old;  // przedosatnio odczytany przyrost polozenie (delta y[k-2]
   // -- mierzone w impulsach)
@@ -81,6 +105,5 @@ class FileCurrentControl : public RTT::TaskContext {
 
   int curr;
   std::vector<double> currents;
-
 };
-#endif
+#endif  // FILECURRENTCONTROL_H_
