5a2f1229b15fb3d1a7f48f59bf958e741ba1a3a7
Bartosz Swistak <bartswis@gmail.com>
2015-04-02 21:44:08 +0200

    fcc upgraded to fic

---
+++ file_input_control/src/FileInputControl.h
@@ -0,0 +1,94 @@
+#ifndef FILEINPUTCONTROL_H_
+#define FILEINPUTONTROL_H_
+
+#include <fstream>
+#include <string>
+
+using namespace std;
+using namespace RTT;
+
+class FileInputControl : public RTT::TaskContext {
+
+ public:
+
+  FileInputControl(const std::string& name);
+  ~FileInputControl();
+
+  int doServo(double, int);
+  int doServo_fcc(double, int);
+  int doServo_fic(double, int);
+  void reset();
+
+ private:
+
+  bool configureHook();
+  void updateHook();
+
+  InputPort<double> desired_position_;
+  InputPort<double> deltaInc_in;
+  InputPort<bool> synchro_state_in_;
+
+  OutputPort<double> computedPwm_out;
+  OutputPort<bool> emergency_stop_out_;
+
+  double desired_position_increment_;
+  double desired_position_old_, desired_position_new_;
+  double deltaIncData;
+
+  bool synchro_state_old_, synchro_state_new_;
+
+  long update_hook_iteration_number_;
+  long new_position_iteration_number_;
+
+  // Properties
+  int reg_number_;
+  bool debug_;
+  double A_;
+  double BB0_;
+  double BB1_;
+  bool current_mode_;
+  double max_output_current_;
+  double current_reg_kp_;
+  double eint_dif_;
+  double max_desired_increment_;
+  double enc_res_;
+
+  std::string input_type_;
+  std::string filename_;
+
+  enum e_type
+  {
+    defalt,
+    current,
+    increment
+  } type;
+
+  double position_increment_old;  // przedosatnio odczytany przyrost polozenie (delta y[k-2]
+  // -- mierzone w impulsach)
+  double position_increment_new;  // ostatnio odczytany przyrost polozenie (delta y[k-1]
+  // -- mierzone w impulsach)
+  double step_old_pulse;  // poprzednia wartosc zadana dla jednego kroku regulacji
+  // (przyrost wartosci zadanej polozenia -- delta r[k-2]
+  // -- mierzone w radianach)
+  double step_new;  // nastepna wartosc zadana dla jednego kroku regulacji
+  // (przyrost wartosci zadanej polozenia -- delta r[k-1]
+  // -- mierzone w radianach)
+  double step_old;  // poprzednia wartosc zadana dla jednego kroku regulacji
+  // (przyrost wartosci zadanej polozenia -- delta r[k-1]
+  // -- mierzone w radianach)
+
+  double set_value_new;  // wielkosc kroku do realizacji przez HI (wypelnienie PWM -- u[k])
+  double set_value_old;  // wielkosc kroku do realizacji przez HI (wypelnienie PWM -- u[k-1])
+  double set_value_very_old;  // wielkosc kroku do realizacji przez HI (wypelnienie PWM -- u[k-2])
+  double delta_eint;  // przyrost calki uchybu
+  double delta_eint_old;  // przyrost calki uchybu w poprzednim kroku
+
+  double output_value;
+
+  double a_, b0_, b1_;
+
+  int idx;
+  std::vector<double> inputs;
+
+};
+#endif
