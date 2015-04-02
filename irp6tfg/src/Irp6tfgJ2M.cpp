c1719ec0d75b925c9324b9f72f516584f53f86ac
Tomek Winiarski <tomrobotics@gmail.com>
2015-04-02 14:45:48 -0700

    style fix

--- irp6tfg/src/Irp6tfgJ2M.cpp
+++ irp6tfg/src/Irp6tfgJ2M.cpp
@@ -1,5 +1,35 @@
-#include <rtt/Component.hpp>
+/*
+ * Copyright (c) 2014-2015, Robot Control and Pattern Recognition Group, Warsaw University of Technology.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Robot Control and Pattern Recognition Group,
+ *       Warsaw University of Technology nor the names of its contributors may
+ *       be used to endorse or promote products derived from this software
+ *       without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
+#include <rtt/Component.hpp>
+#include <string>
 #include "Irp6tfgJ2M.h"
 #include "Irp6tfgTransmission.h"
 
@@ -32,18 +62,16 @@ void Irp6tfgJ2M::updateHook() {
 }
 
 bool Irp6tfgJ2M::i2mp(const double* joints, double* motors) {
-
   // Obliczenie kata obrotu walu silnika napedowego chwytaka.
   motors[0] = inv_a_7 * sqrt(inv_b_7 + inv_c_7 * joints[0]) + inv_d_7;
   return checkMotorPosition(motors);
 }
 
 bool Irp6tfgJ2M::checkMotorPosition(const double * motor_position) {
-
   if (motor_position[0] < LOWER_MOTOR_LIMIT[0])  // Kat f1 mniejszy od minimalnego
-    return false;  //throw NonFatal_error_2(BEYOND_LOWER_LIMIT_AXIS_0);
+    return false;  // throw NonFatal_error_2(BEYOND_LOWER_LIMIT_AXIS_0);
   else if (motor_position[0] > UPPER_MOTOR_LIMIT[0])  // Kat f1 wiekszy od maksymalnego
-    return false;  //throw NonFatal_error_2(BEYOND_UPPER_LIMIT_AXIS_0);
+    return false;  // throw NonFatal_error_2(BEYOND_UPPER_LIMIT_AXIS_0);
 
   return true;
 }
--- sarkofag/src/SarkofagM2J.h
+++ sarkofag/src/SarkofagM2J.h
@@ -1,13 +1,44 @@
-#ifndef SarkofagM2J_H_
-#define SarkofagM2J_H_
+/*
+ * Copyright (c) 2014-2015, Robot Control and Pattern Recognition Group, Warsaw University of Technology.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Robot Control and Pattern Recognition Group,
+ *       Warsaw University of Technology nor the names of its contributors may
+ *       be used to endorse or promote products derived from this software
+ *       without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef SARKOFAGM2J_H_
+#define SARKOFAGM2J_H_
 
 #include <rtt/TaskContext.hpp>
 #include <rtt/Port.hpp>
 #include <Eigen/Dense>
+#include <string>
 
 class SarkofagM2J : public RTT::TaskContext {
  public:
-   SarkofagM2J(const std::string& name);
+  explicit SarkofagM2J(const std::string& name);
   virtual ~SarkofagM2J();
 
   bool configureHook();
@@ -20,4 +51,4 @@ class SarkofagM2J : public RTT::TaskContext {
   Eigen::VectorXd motor_position_, joint_position_;
 };
 
-#endif /* SarkofagM2J */
+#endif  // SARKOFAGM2J_H_
