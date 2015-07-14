d21a3c2d07b46a1567f04604b71cceb6857a47f2
Tomasz Winiarski <tomrobotics@gmail.com>
2015-07-14 17:27:40 +0200

    experimental dashboard in progress

--- rqt_mypkg/src/rqt_mypkg/my_module.py
+++ rqt_mypkg/src/rqt_mypkg/my_module.py
@@ -1,63 +1,33 @@
 import os
 import rospy
+from rosgraph import rosenv
 import rospkg
 
 from qt_gui.plugin import Plugin
 from python_qt_binding import loadUi
 from python_qt_binding.QtGui import QWidget
 
-class MyPlugin(Plugin):
 
-    def __init__(self, context):
-        super(MyPlugin, self).__init__(context)
-        # Give QObjects reasonable names
-        self.setObjectName('MyPlugin')
+from rqt_robot_dashboard.dashboard import Dashboard
+from rqt_robot_dashboard.monitor_dash_widget import MonitorDashWidget
+from rqt_robot_dashboard.console_dash_widget import ConsoleDashWidget
 
-        # Process standalone plugin command-line arguments
-        from argparse import ArgumentParser
-        parser = ArgumentParser()
-        # Add argument(s) to the parser.
-        parser.add_argument("-q", "--quiet", action="store_true",
-                      dest="quiet",
-                      help="Put plugin in silent mode")
-        args, unknowns = parser.parse_known_args(context.argv())
-        if not args.quiet:
-            print 'arguments: ', args
-            print 'unknowns: ', unknowns
+class MyPlugin(Dashboard):
+    def setup(self, context):
+        self.name = 'NAO Dashboard (%s)'%rosenv.get_master_uri()
 
-        # Create QWidget
-        self._widget = QWidget()
-        # Get path to UI file which should be in the "resource" folder of this package
-        ui_file = os.path.join(rospkg.RosPack().get_path('rqt_mypkg'), 'resource', 'MyPlugin.ui')
-        # Extend the widget with all attributes and children from UI file
-        loadUi(ui_file, self._widget)
-        # Give QObjects reasonable names
-        self._widget.setObjectName('MyPluginUi')
-        # Show _widget.windowTitle on left-top of each plugin (when 
-        # it's set in _widget). This is useful when you open multiple 
-        # plugins at once. Also if you open multiple instances of your 
-        # plugin at once, these lines add number to make it easy to 
-        # tell from pane to pane.
-        if context.serial_number() > 1:
-            self._widget.setWindowTitle(self._widget.windowTitle() + (' (%d)' % context.serial_number()))
-        # Add widget to the user interface
-        context.add_widget(self._widget)
+        
+        # Diagnostics
+        self._monitor = MonitorDashWidget(self.context)
 
-    def shutdown_plugin(self):
-        # TODO unregister all publishers here
+        # Rosout
+        self._console = ConsoleDashWidget(self.context, minimal=False)
+
+    def get_widgets(self):
+        return [
+                [self._monitor, self._console]
+               ]
+
+    def shutdown_dashboard(self):
         pass
-
-    def save_settings(self, plugin_settings, instance_settings):
-        # TODO save intrinsic configuration, usually using:
-        # instance_settings.set_value(k, v)
-        pass
-
-    def restore_settings(self, plugin_settings, instance_settings):
-        # TODO restore intrinsic configuration, usually using:
-        # v = instance_settings.value(k)
-        pass
-
-    #def trigger_configuration(self):
-        # Comment in to signal that the plugin has a way to configure
-        # This will enable a setting button (gear icon) in each dock widget title bar
-        # Usually used to open a modal configuration dialog
+	
