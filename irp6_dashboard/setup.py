b82d21344f9bf1a473b265d8142c5ff114f2ac9f
Tomek Winiarski <tomrobotics@gmail.com>
2015-07-15 09:12:26 -0700

    irp6_dashboard cleanups

--- irp6_dashboard/setup.py
+++ irp6_dashboard/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['nao_dashboard'],
+    packages=['irp6_dashboard'],
     package_dir={'': 'src'},
 )
 
