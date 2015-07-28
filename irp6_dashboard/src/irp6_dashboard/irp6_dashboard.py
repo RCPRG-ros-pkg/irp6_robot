4a829f43861f6c50459a82342e9e907693afc3f9
Tomasz Winiarski <tomrobotics@gmail.com>
2015-07-28 20:08:20 +0200

    rqt_dashboard diagnostic message added

--- irp6_dashboard/src/irp6_dashboard/irp6_dashboard.py
+++ irp6_dashboard/src/irp6_dashboard/irp6_dashboard.py
@@ -3,6 +3,7 @@ import rospy
 from rosgraph import rosenv
 import rospkg
 
+from diagnostic_msgs.msg import DiagnosticArray, DiagnosticStatus
 from qt_gui.plugin import Plugin
 from python_qt_binding import loadUi
 from python_qt_binding.QtGui import QWidget
@@ -26,11 +27,32 @@ class Irp6Dashboard(Dashboard):
         
         ## Motors
         self._motors_button = Motors(self.context)
+        
+        self._agg_sub = rospy.Subscriber('diagnostics_agg', DiagnosticArray, self.new_diagnostic_message)
 
     def get_widgets(self):
         return [
                 [self._monitor, self._console, self._motors_button]
                ]
+
+    def shutdown_dashboard(self):
+        self._agg_sub.unregister()
+
+    def new_diagnostic_message(self, msg):
+        """
+        callback to process dashboard_agg messages
+        :param msg: dashboard_agg DashboardState message
+        :type msg: pr2_msgs.msg.DashboardState
+        """
+        self._dashboard_message = msg
+        for status in msg.status:
+            if status.name == 'Hardware Interface':
+                for kv in status.values:
+                     if kv.key == 'Synchro':
+                         if kv.value == 'TRUE':
+                             pass
+                             # my_state = "Synchro true"
+                             # print my_state
 
     def shutdown_dashboard(self):
         pass
