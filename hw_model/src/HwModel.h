888efe620d192a4f99b9568020dbc00abbda7fa8
Tomasz Winiarski <tomrobotics@gmail.com>
2015-11-13 14:24:23 +0100

    hardware model

---
+++ hw_model/src/HwModel.h
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2014-2015, Robot Control and Pattern Recognition Group, Warsaw University of Technology.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Robot Control and Pattern Recognition Group,
+ *       Warsaw University of Technology nor the names of its contributors may
+ *       be used to endorse or promote products derived from this software
+ *       without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef HWMODEL_H_
+#define HWMODEL_H_
+
+#include <rtt/TaskContext.hpp>
+#include <rtt/Port.hpp>
+#include <Eigen/Dense>
+#include <string>
+#include <vector>
+
+#define NUMBER_OF_SERVOS 7
+
+
+class HwModel : public RTT::TaskContext {
+ public:
+  explicit HwModel(const std::string& name);
+  virtual ~HwModel();
+
+  bool configureHook();
+  void updateHook();
+ private:
+  bool i2mp(const double* joints, double* motors);
+
+  RTT::InputPort<Eigen::VectorXd> port_joint_position_;
+  RTT::OutputPort<Eigen::VectorXd> port_motor_position_;
+
+  Eigen::VectorXd motor_position_, joint_position_;
+
+  double SYNCHRO_JOINT_POSITION[NUMBER_OF_SERVOS];
+
+  // properties
+  std::vector<double> synchro_motor_position_;
+};
+
+#endif  // HWMODEL_H_
