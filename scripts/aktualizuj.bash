89611c704deefd742f5507527e09e708a053c847
Tomek Winiarski <tomrobotics@gmail.com>
2015-12-02 14:45:11 -0800

    fix

--- scripts/aktualizuj.bash
+++ scripts/aktualizuj.bash
@@ -7,8 +7,8 @@ then
 	exit
 fi
 
-wget https://raw.githubusercontent.com/RCPRG-ros-pkg/sarkofag_robot/master/scripts/irp6.rosinstall -O /tmp/irp6.rosinstall
-wget https://raw.githubusercontent.com/RCPRG-ros-pkg/sarkofag_robot/master/scripts/update_and_compile.bash -O /tmp/update_and_compile.bash
+wget https://raw.githubusercontent.com/RCPRG-ros-pkg/irp6_robot/master/scripts/irp6.rosinstall -O /tmp/irp6.rosinstall
+wget https://raw.githubusercontent.com/RCPRG-ros-pkg/irp6_robot/master/scripts/update_and_compile.bash -O /tmp/update_and_compile.bash
 
 
 cd ../../../../
