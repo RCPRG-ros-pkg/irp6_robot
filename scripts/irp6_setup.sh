3b247dbd7985c32ceed37e9a7b27f17fb8ba7e4a
Tomasz Winiarski <tomrobotics@gmail.com>
2016-05-31 15:02:04 -0700

    scripts modifications

--- scripts/irp6_setup.sh
+++ scripts/irp6_setup.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
-
-
-
-if [ "x$ROS_MASTER_URI" == "x" ]
-then
-	echo "Please source ros"
-	exit
-fi
-
-wget https://raw.githubusercontent.com/RCPRG-ros-pkg/irp6_robot/master/scripts/irp6.rosinstall -O /tmp/irp6.rosinstall
-wget https://raw.githubusercontent.com/RCPRG-ros-pkg/irp6_robot/master/scripts/update_and_compile.bash -O /tmp/update_and_compile.bash
+bash init.bash
 
 if [ ! -d $1 ]; then
   mkdir $1
 fi
+
+cp var.cfg /tmp
 
 cd $1
 wstool init
