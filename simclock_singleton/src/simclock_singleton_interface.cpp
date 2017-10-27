c24c6e9f821303d8426c6b6b6ca43551844321c3
Tomasz Winiarski <tomrobotics@gmail.com>
2017-10-27 23:44:22 +0200

    simclock singleton improovments

--- simclock_singleton/src/simclock_singleton_interface.cpp
+++ simclock_singleton/src/simclock_singleton_interface.cpp
@@ -34,10 +34,6 @@
 #include <simclock_singleton/simclock_singleton.h>
 #include <simclock_singleton/simclock_singleton_interface.h>
 
-namespace simclock_singleton {
-boost::shared_ptr<simclock_singleton::SimClockSingleton> sim_clock_thread;
-}
-
 const bool simclock_singleton::register_robot_active(const int robot_code) {
   return SimClockSingleton::Instance()->registerRobotActive(robot_code);
 }
